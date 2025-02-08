6d120ce580615e70b9fc58d03aab8c85e3351e6c
hippietrail <hippytrail@gmail.com>
2025-02-08 19:15:01 +0800

    msessing around

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::error::Error;
 use quick_xml::events::{Event, BytesStart};
 use quick_xml::reader::Reader;
 
+use std::io::{self, BufRead, Write};
+
 fn increment_counters(seen: &mut HashMap<String, usize>, key: String) {
     if let Some(value) = seen.get_mut(&key) {
         (*value) += 1;
@@ -15,7 +17,10 @@ fn increment_counters(seen: &mut HashMap<String, usize>, key: String) {
 fn process_attributes(seen: &mut HashMap<String, usize>, path: &mut Vec<String>, node: BytesStart) {
     path.push("@attribute".into());
 
+    println!("Entering process_attributes with node: {:?}", node);
+
     for attribute in node.attributes() {
+        println!("Processing attribute: {:?}", attribute);
         path.push(format!(
             "{:?}",
             String::from_utf8(attribute.unwrap().key.0.to_vec()).unwrap()
@@ -40,8 +45,8 @@ fn print_results(seen: &HashMap<String, usize>, counter: &usize) {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let filename = "/home/vscode/enwiki-20230501-pages-meta-history1.xml-p1p844";
-    let mut reader = Reader::from_file(filename).unwrap();
+    let stdin = io::stdin();
+    let mut reader = Reader::from_reader(stdin.lock());
 
     let mut path = Vec::new();
     let mut seen = HashMap::new();
@@ -49,30 +54,44 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = Vec::new();
     let mut counter = 0;
 
+    let mut event_count = 0;
+
     loop {
         match reader.read_event_into(&mut buffer) {
             Err(error) => break println!("{}", error),
             Ok(Event::Eof) => break println!("Completed."),
             Ok(Event::Start(node)) => {
+                // println!("Start element: {:?}", node);
+                std::io::stdout().flush().unwrap();
                 path.push(format!("{:?}", String::from_utf8(node.name().0.to_vec()).unwrap()));
                 increment_counters(&mut seen, path.join(" / "));
                 process_attributes(&mut seen, &mut path, node);
+                event_count += 1;
+                if event_count >= 100000 {
+                    break println!("Reached 100000 events");
+                }
             }
             Ok(Event::End(_)) => {
+                // println!("End element");
                 path.pop();
             }
             Ok(Event::Text(_)) => {
+                // println!("Text element");
                 path.push("@text".into());
                 increment_counters(&mut seen, path.join(" / "));
                 path.pop();
             }
             Ok(Event::Empty(node)) => {
+                // println!("Empty element: {:?}", node);
                 path.push(format!("{:?}", String::from_utf8(node.name().0.to_vec()).unwrap()));
                 increment_counters(&mut seen, path.join(" / "));
                 process_attributes(&mut seen, &mut path, node);
                 path.pop();
             }
-            Ok(_) => counter += 1,
+            Ok(_) => {
+                println!("Other element");
+                counter += 1;
+            }
         }
 
         buffer.clear();
