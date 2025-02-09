3f0dd0bd98a5e77b5cd4cb015748c57e44e59357
hippietrail <hippytrail@gmail.com>
2025-02-09 13:59:48 +0800

    more optionals

--- src/main.rs
+++ src/main.rs
@@ -11,52 +11,53 @@ fn get_node_name_end(node: &quick_xml::events::BytesEnd) -> String {
     String::from_utf8(node.name().0.to_vec()).unwrap()
 }
 
-fn start_namespace(node: &quick_xml::events::BytesStart, last_ns_key: &mut i32, last_text_content: &mut Option<String>) {
+fn start_namespace(node: &quick_xml::events::BytesStart,
+    ns_key: &mut Option<i32>,
+    last_text_content: &mut Option<String>)
+{
     if let Some(att) = node.attributes().find(|a| a.as_ref().unwrap().key == quick_xml::name::QName(b"key")) {
-        *last_ns_key = String::from_utf8(att.unwrap().value.to_vec()).unwrap().parse::<i32>().unwrap();
+        *ns_key = Some(String::from_utf8(att.unwrap().value.to_vec()).unwrap().parse::<i32>().unwrap());
     }
     *last_text_content = None; // Reset for each namespace
 }
 
-fn end_namespace(last_ns_key: i32, last_text_content: &Option<String>) {
+fn end_namespace(ns_key: Option<i32>, last_text_content: &Option<String>) {
     // The default namespace, 0, has no name
     let ns_text = last_text_content.as_ref().unwrap_or(&String::from("")).clone();
-    println!("namespace {} : \"{}\"", last_ns_key, ns_text);
+    println!("namespace {} : \"{}\"", ns_key.unwrap(), ns_text);
 }
 
-fn start_page(last_page_title: &mut String, last_page_ns: &mut i32, last_page_id: &mut Option<i32>) {
-    *last_page_title = String::new();
-    *last_page_ns = -555;
-    *last_page_id = None;
+fn start_page(page_title: &mut String, page_ns: &mut Option<i32>, page_id: &mut Option<i32>, page_rev_id: &mut Option<i32>) {
+    *page_title = String::new();
+    *page_ns = None;
+    *page_id = None;
+    *page_rev_id = None;
 }
 
 fn start_page_title(last_text_content: &mut Option<String>) {
-    *last_text_content = None; // Reset text content for the page title
+    *last_text_content = None;
 }
 
-fn end_page_title(last_page_title: &mut String, last_text_content: &mut Option<String>) {
-    *last_page_title = last_text_content.take().unwrap_or_default(); // Default to empty string if None
+fn end_page_title(page_title: &mut String, last_text_content: &mut Option<String>) {
+    *page_title = last_text_content.take().unwrap_or_default();
 }
 
-fn start_page_ns(last_text_content: &mut Option<String>, last_page_ns: &mut i32) {
-    *last_page_ns = -777; // Placeholder for actual logic
-    *last_text_content = None; // Reset for each namespace
+fn start_page_ns(last_text_content: &mut Option<String>, page_ns: &mut Option<i32>) {
+    *page_ns = None;
+    *last_text_content = None;
 }
 
-fn end_page_ns(last_page_ns: &mut i32, last_text_content: &mut Option<String>) {
-    let ns_text = last_text_content.take().unwrap_or_default(); // Default to empty string if None
-    *last_page_ns = ns_text.parse::<i32>().unwrap(); // Parse ns text to i32
+fn end_page_ns(page_ns: &mut Option<i32>, last_text_content: &mut Option<String>) {
+    let ns_text = last_text_content.take().unwrap_or_default();
+    *page_ns = ns_text.parse::<i32>().ok();
 }
 
-fn start_id(last_text_content: &mut Option<String>/*, last_id: &mut Option<i32>*/) {
-    // *last_id = None; // Placeholder for actual logic
-    *last_text_content = None; // Reset for each namespace
+fn start_id(last_text_content: &mut Option<String>) {
+    *last_text_content = None;
 }
 
-fn end_id(/*last_id: &mut Option<i32>, */page_id: &mut Option<i32>, page_rev_id: &mut Option<i32>, page_rev_contrib_id: &mut Option<i32>, last_text_content: &mut Option<String>) {
+fn end_id(page_id: &mut Option<i32>, page_rev_id: &mut Option<i32>, page_rev_contrib_id: &mut Option<i32>, last_text_content: &mut Option<String>) {
     let id = last_text_content.take().unwrap_or_default().parse::<i32>().unwrap();
-    // *last_id = Some(id); // Parse ns text to i32
-    // out of the page, rev, and contrib id, write this into the first of them that is None
     if page_id.is_none() {
         *page_id = Some(id);
     } else if page_rev_id.is_none() {
@@ -66,8 +67,8 @@ fn end_id(/*last_id: &mut Option<i32>, */page_id: &mut Option<i32>, page_rev_id:
     }
 }
 
-fn end_page(last_page_title: &String, last_page_ns: i32, last_page_id: Option<i32>) {
-    println!("page id {} -> {} : \"{}\"", last_page_id.unwrap(), last_page_ns, last_page_title);
+fn end_page(page_title: &String, page_ns: Option<i32>, page_id: Option<i32>) {
+    println!("page id {} -> {} : \"{}\"", page_id.unwrap(), page_ns.unwrap(), page_title);
 }
 
 #[tokio::main]
@@ -76,10 +77,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let mut reader = Reader::from_reader(stdin.lock());
 
     let mut last_text_content: Option<String> = None;
-    let mut last_ns_key: i32 = -333;
-    let mut last_page_title: String = String::new();
-    let mut last_page_ns: i32 = -444;
-    // let mut last_id: Option<i32> = None;
+    let mut ns_key: Option<i32> = None;
+    let mut page_title: String = String::new();
+    let mut page_ns: Option<i32> = None;
     let mut page_id: Option<i32> = None;
     let mut page_rev_id: Option<i32> = None;
     let mut page_rev_contrib_id: Option<i32> = None;
@@ -89,26 +89,26 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         match reader.read_event_into(&mut buffer) {
             Ok(Event::Start(node)) => match get_node_name(&node).as_str() {
-                "namespace" => start_namespace(&node, &mut last_ns_key, &mut last_text_content),
-                "page" => start_page(&mut last_page_title, &mut last_page_ns, &mut page_id),
+                "namespace" => start_namespace(&node, &mut ns_key, &mut last_text_content),
+                "page" => start_page(&mut page_title, &mut page_ns, &mut page_id, &mut page_rev_id),
                 "title" => start_page_title(&mut last_text_content),
-                "ns" => start_page_ns(&mut last_text_content, &mut last_page_ns),
-                "id" => start_id(&mut last_text_content/*, &mut last_id*/),
+                "ns" => start_page_ns(&mut last_text_content, &mut page_ns),
+                "id" => start_id(&mut last_text_content),
                 _ => {}
             },
             Ok(Event::Empty(node)) => match get_node_name(&node).as_str() {
                 "namespace" => {
-                    start_namespace(&node, &mut last_ns_key, &mut last_text_content);
-                    end_namespace(last_ns_key, &last_text_content);
+                    start_namespace(&node, &mut ns_key, &mut last_text_content);
+                    end_namespace(ns_key, &last_text_content);
                 },
                 _ => {}
             },
             Ok(Event::End(node)) => match get_node_name_end(&node).as_str() {
-                "namespace" => end_namespace(last_ns_key, &last_text_content),
-                "title" => end_page_title(&mut last_page_title, &mut last_text_content),
-                "ns" => end_page_ns(&mut last_page_ns, &mut last_text_content),
-                "id" => end_id(/*&mut last_id, */&mut page_id, &mut page_rev_id, &mut page_rev_contrib_id, &mut last_text_content),
-                "page" => end_page(&last_page_title, last_page_ns, page_id),
+                "namespace" => end_namespace(ns_key, &last_text_content),
+                "title" => end_page_title(&mut page_title, &mut last_text_content),
+                "ns" => end_page_ns(&mut page_ns, &mut last_text_content),
+                "id" => end_id(&mut page_id, &mut page_rev_id, &mut page_rev_contrib_id, &mut last_text_content),
+                "page" => end_page(&page_title, page_ns, page_id),
                 _ => {}
             },
             Ok(Event::Text(text)) => {
