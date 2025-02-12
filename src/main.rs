781609c33f3f95a5fff9ed999d8b0ae773d4bfd7
hippietrail <hippytrail@gmail.com>
2025-02-12 15:04:03 +0800

    outputs xml with regular updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::error::Error;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
@@ -25,13 +26,8 @@ fn end_namespace(ns_key: Option<i32>, last_text_content: &Option<String>) {
     // println!("namespace {} : \"{}\"", ns_key.unwrap(), ns_text);
 }
 
-fn start_page(
-    page_title: &mut String,
-    page_ns: &mut Option<i32>,
-    page_id: &mut Option<i32>,
-    page_rev_id: &mut Option<i32>,
-    page_rev_text: &mut String,
-) {
+fn start_page(page_title: &mut String, page_ns: &mut Option<i32>,
+        page_id: &mut Option<i32>, page_rev_id: &mut Option<i32>, page_rev_text: &mut String) {
     *page_title = String::new();
     *page_ns = None;
     *page_id = None;
@@ -80,29 +76,152 @@ fn end_page_rev_text(page_rev_text: &mut String, last_text_content: &mut Option<
     *page_rev_text = last_text_content.take().unwrap_or_default();
 }
 
-fn end_page(page_title: &String, page_ns: Option<i32>, page_id: Option<i32>, page_rev_id: Option<i32>, page_rev_text: &String) {
-    // println!("page page/rev id {}/{} -> {} : \"{}\"", page_id.unwrap(), page_rev_id.unwrap(), page_ns.unwrap(), page_title);
-    if page_ns.unwrap() == 0 {
-        // Regex to match level-2 headings
-        let heading_regex = Regex::new(r"^== ?([^=]*?) ?==$").unwrap();
+fn end_page(title: &String, namespace: Option<i32>, page_id: Option<i32>, rev_id: Option<i32>, text: &String,
+        page_num: &mut u64, section_num: &mut u64, just_emitted_update: &mut bool, headings_seen: &mut HashMap<String, u64>) {
+    if namespace.unwrap() == 0 {
+        let all_lang_headings_regex = Regex::new(r"(?m)^== ?([^=]*?) ?== *$\n").unwrap();
+        let our_lang_headings_regex = Regex::new(r"(?m)^== ?(English|Translingual) ?== *$\n").unwrap();
+        let mut lang_headings: Vec<String> = Vec::new();
         let mut languages: Vec<String> = Vec::new();
 
-        for line in page_rev_text.lines() {
-            if let Some(captures) = heading_regex.captures(line) {
-                if let Some(heading) = captures.get(1) {
-                    languages.push(heading.as_str().to_string());
+        for capture in all_lang_headings_regex.captures_iter(text) {
+            if let (Some(heading), Some(lang)) = (capture.get(0), capture.get(1)) {
+                let heading_string = heading.as_str().to_string();
+                let lang_string = lang.as_str().to_string();
+
+                lang_headings.push(heading_string);
+                languages.push(lang_string);
+            }
+        }
+        
+        languages.retain(|lang| lang == "English" || lang == "Translingual");
+
+        if languages.len() == 0 { return }
+
+        *page_num += 1;
+
+        let mut page_xml = format!("  <p n=\"{}\" pid=\"{}\" rid=\"{}\">\n    <t>{}</t>",
+            page_num, page_id.unwrap(), rev_id.unwrap(), title);
+
+        // now split the text by the same regex
+        let splitted = our_lang_headings_regex.split(text).collect::<Vec<&str>>();
+
+        let mut sections_xml_vec: Vec<String> = Vec::new();
+
+        for (i, section) in splitted.iter().enumerate().skip(1) {
+            *section_num += 1;
+
+            let mut section_xml = format!("    <s num=\"{}\" lang=\"{}\">", section_num, languages[i-1]);
+            let mut inner_section = *section;
+
+            if let Some(heading) = all_lang_headings_regex.find(section) {
+                inner_section = &section[0..heading.start()];
+            }
+            // let chosen_stuff = inner_section;
+
+            let all_headings_regex = Regex::new(r"(?m)^(===+) ?([^=]*?) ?===+ *$\n").unwrap();
+            let mut headings: Vec<(String, u8)> = Vec::new();
+
+            for capture in all_headings_regex.captures_iter(inner_section) {
+                let heading_depth = capture.get(1).unwrap().as_str().len();
+                let heading_name = capture.get(2).unwrap();
+                let name_string = heading_name.as_str().to_string();
+
+                headings.push((name_string, heading_depth.try_into().unwrap()));
+            }
+
+            let heading_blacklist = [
+                "Anagrams",
+                "Antonyms",
+                "Collocations",
+                "Conjugation",
+                "Coordinate terms",
+                "Derived characters",
+                "Derived terms",
+                "Descendants",
+                // "Etymology", // keep because there can be multiple
+                "Further reading",
+                "Gallery",
+                "Han character",
+                "Holonyms",
+                "Hypernyms",
+                "Hyponyms",
+                "Letter",
+                "Meronyms",
+                "Number",
+                "Phrase",
+                "Pronunciation",
+                "Quotations",
+                "References",
+                "Related characters",
+                "Related terms",
+                "See also",
+                "Statistics",
+                "Symbol",
+                "Synonyms",
+                "Translations",
+                "Trivia",
+                "Unrelated terms",
+                "Usage notes",
+            ];
+
+            headings.retain(|heading| !heading_blacklist.contains(&heading.0.as_str()));
+
+            if headings.len() > 0 {
+                let chosen_stuff = "\n".to_owned() + &headings
+                    .iter()
+                    .map(|heading| format!("{:width$}{}", "", heading.0, width = heading.1 as usize * 2 + 2))
+                    .collect::<Vec<String>>()
+                    .join("\n");
+
+                // update seen heading count
+                for heading in &headings {
+                    *headings_seen.entry(heading.0.clone()).or_insert(0) += 1;
                 }
+
+                section_xml += "\n";
+                section_xml += &format!("      <x>{}{}</x>",
+                    "", //lang_headings[i-1],
+                    chosen_stuff);
+                section_xml += "\n";
+                section_xml += "    </s>";
+
+                if chosen_stuff.len() == 0 {
+                    eprintln!("** have headings but no stuff chosen **")
+                }
+
+                sections_xml_vec.push(section_xml);
             }
         }
 
-        // filter out all languages other than 'English' and 'Translingual'
-        languages.retain(|lang| lang == "English" || lang == "Translingual");
+        if sections_xml_vec.len() > 0 {
+            page_xml += "\n";
+            page_xml += &sections_xml_vec.join("\n");
 
-        if languages.len() > 0 {
-            println!("{} >>> Languages: {}", page_title, languages.join(", "));
-        } else {
+            page_xml += "\n";
+            page_xml += "  </p>";
+            println!("{}", page_xml);
+
+            // every n pages, emit an update
+            if *page_num %256 == 0 {
+                emit_update(&headings_seen);
+                *just_emitted_update = true;
+            } else {
+                *just_emitted_update = false;
+            }
         }
     }
+}
+
+fn emit_update(headings_seen: &HashMap<String, u64>) {
+    println!("  <update>");
+    let mut sorted_headings: Vec<_> = headings_seen.iter().collect();
+    sorted_headings.sort_by(|a, b| b.1.cmp(a.1));
+    
+    for (heading, count) in sorted_headings {
+        println!("    <h n=\"{}\" c=\"{}\"/>", heading, count);
+    }
+    println!("  </update>");
 }
 
 #[tokio::main]
@@ -121,7 +240,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut buffer = Vec::new();
 
+    let mut page_num: u64 = 0;
+    let mut section_num: u64 = 0;
+
+    // a map of strings (of some kind, &str or string or whatever) to u64 to count how many times we've seen each heading
+    let mut headings_seen: HashMap<String, u64> = HashMap::new();
+
+    println!("<wiktionary>");
+
+    let mut just_emitted_update = false;
+
     loop {
+    // while page_num < 3 {
         match reader.read_event_into(&mut buffer) {
             Ok(Event::Start(node)) => match get_node_name(&node).as_str() {
                 "namespace" => start_namespace(&node, &mut ns_key, &mut last_text_content),
@@ -145,7 +275,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 "ns" => end_page_ns(&mut page_ns, &mut last_text_content),
                 "id" => end_id(&mut page_id, &mut page_rev_id, &mut page_rev_contrib_id, &mut last_text_content),
                 "text" => end_page_rev_text(&mut page_rev_text, &mut last_text_content),
-                "page" => end_page(&page_title, page_ns, page_id, page_rev_id, &page_rev_text),
+                "page" => end_page(&page_title, page_ns, page_id, page_rev_id, &page_rev_text,
+                    &mut page_num, &mut section_num, &mut just_emitted_update, &mut headings_seen),
                 _ => {}
             },
             Ok(Event::Text(text)) => {
@@ -156,14 +287,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     last_text_content = Some(s);
                 }
             }
-            Ok(Event::Eof) => break println!("Completed."),
+            Ok(Event::Eof) => {}
             Ok(_) => {}
-            Err(error) => break println!("{}", error),
+            Err(error) => break //println!("{}", error),
         }
 
         // Clear the buffer for the next event
         buffer.clear();
     }
+
+    if !just_emitted_update {
+        emit_update(&headings_seen);
+    }
+
+    println!("</wiktionary>");
 
     Ok(())
 }
